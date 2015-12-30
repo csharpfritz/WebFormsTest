419e33b634d9991e91d3bb63aad58f4a51bea747
Jeffrey T. Fritz <jeffreytfritz@gmail.com>
2015-12-30 09:14:13 -0500

    Added FirstTest sample solution

---
+++ Samples/FirstTest/FirstTest.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FirstTest.Tests
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+
+      // Arrange
+
+      // Act
+      var sut = new FirstTest.Default();
+      sut.FireEvent(WebFormsTest.TestablePage.WebFormEvent.Load, EventArgs.Empty);
+
+      // Assert
+      Assert.IsTrue(sut.LoadEventTriggered, "Load event was not triggered");
+
+    }
+  }
+}
---
+++ Samples/FirstTest/FirstTest/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FirstTest
+{
+  public partial class Default : WebFormsTest.TestablePage
+  {
+
+    public Default()
+    {
+      Load += Page_Load;
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      LoadEventTriggered = true;
+    }
+
+    public bool LoadEventTriggered = false;
+
+  }
+}

225245ecfced3d392d5d1e3cc091bbf53f9fa575
Doug Mason <demason@mac.com>
2016-09-21 17:21:36 -0500

    Throwing exception when failing to create HttpApplication instance from the user's application type so that its not mysterious when the WebApplicationProxy.Create method fails when its no fault of its own. (#50)

---
+++ WebFormsTest/WebApplicationProxyException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Fritz.WebFormsTest
+{
+    public class WebApplicationProxyException : ApplicationException
+    {
+        public WebApplicationProxyException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
